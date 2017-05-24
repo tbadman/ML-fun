ddc2d6f0e359ed20654d4b4b725a0d370b878569
tbadman <tbadman@gmail.com>
2017-05-24 13:11:10 -0400

    ML experiments

---
+++ titanic_predictor.py
@@ -0,0 +1,104 @@
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import make_scorer, accuracy_score
+from sklearn.model_selection import GridSearchCV
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def encode_features(df_train, df_test):
+    features = ['Fare', 'Cabin', 'Age', 'Sex', 'Lname', 'NamePrefix']
+    df_combined = pd.concat([df_train[features], df_test[features]])
+    
+    for feature in features:
+        le = preprocessing.LabelEncoder()
+        le = le.fit(df_combined[feature])
+        df_train[feature] = le.transform(df_train[feature])
+        df_test[feature] = le.transform(df_test[feature])
+    return df_train, df_test
+
+def simplify_ages(df):
+    df.Age = df.Age.fillna(-0.5)
+    bins = (-1, 0, 5, 12, 18, 25, 35, 60, 120)
+    group_names = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+    categories = pd.cut(df.Age, bins, labels=group_names)
+    df.Age = categories
+    return df
+
+def simplify_cabins(df):
+    df.Cabin = df.Cabin.fillna('N')
+    df.Cabin = df.Cabin.apply(lambda x: x[0])
+    return df
+
+def simplify_fares(df):
+    df.Fare = df.Fare.fillna(-0.5)
+    bins = (-1, 0, 8, 15, 31, 1000)
+    group_names = ['Unknown', '1_quartile', '2_quartile', '3_quartile', '4_quartile']
+    categories = pd.cut(df.Fare, bins, labels=group_names)
+    df.Fare = categories
+    return df
+
+def format_name(df):
+    df['Lname'] = df.Name.apply(lambda x: x.split(' ')[0])
+    df['NamePrefix'] = df.Name.apply(lambda x: x.split(' ')[1])
+    return df    
+    
+def drop_features(df):
+    return df.drop(['Ticket', 'Name', 'Embarked'], axis=1)
+
+def transform_features(df):
+    df = simplify_ages(df)
+    df = simplify_cabins(df)
+    df = simplify_fares(df)
+    df = format_name(df)
+    df = drop_features(df)
+    return df
+
+
+
+
+data_train = pd.read_csv('titanic_train.csv')
+data_test = pd.read_csv('titanic_test.csv')
+
+data_train = transform_features(data_train)
+data_test = transform_features(data_test)
+
+
+data_train, data_test = encode_features(data_train, data_test)
+
+
+X_all = data_train.drop(['Survived', 'PassengerId'], axis=1)
+y_all = data_train['Survived']
+
+num_test = 0.20
+X_train, X_test, y_train, y_test = train_test_split(X_all, y_all, test_size=num_test, random_state=23)
+
+
+# Choose the type of classifier. 
+clf = RandomForestClassifier()
+
+# Choose some parameter combinations to try
+parameters = {'n_estimators': [4, 6, 9], 
+              'criterion': ['entropy', 'gini'],
+              'max_depth': [2, 3, 5, 10], 
+              'min_samples_split': [2, 3, 5],
+              'min_samples_leaf': [1,5,8]
+             }
+
+# Type of scoring used to compare parameter combinations
+acc_scorer = make_scorer(accuracy_score)
+
+# Run the grid search
+grid_obj = GridSearchCV(clf, parameters, scoring=acc_scorer, n_jobs=-1)
+grid_obj = grid_obj.fit(X_train, y_train)
+
+# Set the clf to the best combination of parameters
+clf = grid_obj.best_estimator_
+
+# Fit the best algorithm to the data. 
+clf.fit(X_train, y_train)
+
+
+predictions = clf.predict(X_test)
+print(accuracy_score(y_test, predictions))
